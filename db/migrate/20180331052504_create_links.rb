7076faf54fe4404dd50ad1ce1146577c36539126
Hoseong Son <lsookcha@me.com>
2018-03-31 14:25:08 +0900

    Add migration which creates 'links'

---
+++ db/migrate/20180331052504_create_links.rb
@@ -0,0 +1,10 @@
+class CreateLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.integer. :visit
+
+      t.timestamps
+    end
+  end
+end
