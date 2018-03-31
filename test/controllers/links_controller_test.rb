d2a5085fd08af97d78ba329ce51d6cf58bb7fb5d
Hoseong Son <lsookcha@me.com>
2018-03-31 16:52:42 +0900

    Edit testing code when access exisiting url

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -4,6 +4,15 @@ class LinksControllerTest < ActionDispatch::IntegrationTest
   test "should make url into database" do
     post "http://localhost:3000/register.json?url=google.com"
     assert @response.body
+    assert @response.status, "201"
+    assert_response :success
+  end
+
+  test "should redirect to original url" do
+    post "http://localhost:3000/register.json?url=google.com"
+    post "http://localhost:3000/register.json?url=google.com"
+    assert @response.status, "200"
+    assert @response.body
     assert_response :success
   end
 end
