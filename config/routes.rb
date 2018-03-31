5a4795d84c709196bda0e45385406548cd310af3
Hoseong Son <lsookcha@me.com>
2018-03-31 14:21:31 +0900

    Add Register controller and function 'create'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'stat/show'
+
+  get 'register/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
