927b54a5cc77dbe40ff60c7e66335d60c56b0d50
Hoseong Son <lsookcha@me.com>
2018-03-31 16:17:27 +0900

    Get hourly visit count and show that data

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,15 @@
 class StatsController < ApplicationController
   def index
-    link = Stat.where(link_id: params[:id]).all
+    links = Stat.where(link_id: params[:id]).all.group_by{ | record | record.created_at.year.to_s + "-" + record.created_at.month.to_s + "-" + record.created_at.day.to_s + " " + record.created_at.hour.to_s }
+    data = { Stats: [] }
 
-    if link != nil
-      render :json => link
+    links.keys.each do |key|
+      time_data = {at: key, visits: links[key].count}
+      data[:Stats].push(time_data)
+    end
+
+    if links != nil
+      render :json => data
     else
       render :json => "{msg: 'Not Found'}"
     end
