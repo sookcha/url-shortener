0f89ee9771cc4421563755493d6bb4b774e3a064
Hoseong Son <lsookcha@me.com>
2018-03-31 14:30:13 +0900

    Adding routing endpoints

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   def index
+    render :json => Link.all
   end
 
   def show
--- db/migrate/20180331052504_create_links.rb
+++ db/migrate/20180331052504_create_links.rb
@@ -2,7 +2,7 @@ class CreateLinks < ActiveRecord::Migration[5.1]
   def change
     create_table :links do |t|
       t.string :url
-      t.integer. :visit
+      t.integer :visit
 
       t.timestamps
     end
